ea112b718c1c1b6664523441f4359f835c8d75f2
vuvandinh2000 <vuvandinh2000@gmail.com>
2023-05-30 10:55:17 +0700

    Fix flake8 whitespace

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -30,7 +30,7 @@ class ModelTests(TestCase):
         ]
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(
-                email=email, 
+                email=email,
                 password='sample123',
                 )
             self.assertEqual(user.email, expected)
