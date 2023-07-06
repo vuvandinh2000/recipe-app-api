9ee2ba907abe6dd6b3468942a259034bef300262
Vu Van Dinh <vuvandinh2000@gmail.com>
2023-07-06 23:28:44 +0700

    Add upload image API

--- app/app/settings.py
+++ app/app/settings.py
@@ -143,3 +143,8 @@ AUTH_USER_MODEL = 'core.User'
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+
+# cho phep upload thong qua browser
+SPECTACULAR_SETTINGS = {
+    'COMPONENT_SPLIT_REQUEST': True,
+}
